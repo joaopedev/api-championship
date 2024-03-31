45c27aa42362bb12375c4f552cdef78a97969413
joaopedev <joaopedev@outlook.com>
2024-03-30 21:23:48 -0300

    Finish users and auth controlls.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { PrimaryGeneratedColumn } from 'typeorm';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  cpf: string;
+
+  @Prop()
+  sex: string;
+
+  @Prop()
+  birth: string;
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  otp: string;
+
+  @Prop()
+  emailVerifed: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, ForbiddenException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { encodePassword } from 'src/utils/bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { name, username, email, cpf, password, sex, birth } = createUserDto;
+
+    try {
+      const cpfExists = await this.userModel.findOne({ cpf }).exec();
+      const emailExists = await this.userModel.findOne({ email }).exec();
+      const userNameExists = await this.userModel.findOne({ username }).exec();
+
+      if (cpfExists || emailExists || userNameExists) {
+        throw new BadRequestException(
+          `O username: ${username} e/ou email: ${email} e/ou cpf: ${cpf} já existe no banco de dados.`,
+        );
+      }
+
+      let hashedPassword = encodePassword(createUserDto.password);
+      hashedPassword = password;
+      await this.userModel.create({
+        name,
+        username,
+        email,
+        password,
+        cpf,
+        sex,
+        birth
+      })
+
+      const newUser = new this.userModel({
+        ...createUserDto,
+        password: hashedPassword,
+      });
+
+      return await newUser.save();
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.userModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return await this.userModel.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findUserByUsername(username: string) {
+    return this.userModel.findOne({ username });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    try {
+      const userId = await this.userModel.findOne({ id }).exec();
+
+      if (!userId) {
+        throw new ForbiddenException('Usuário não encontrado.');
+      }
+      return this.userModel.findByIdAndUpdate(
+        {
+          _id: id,
+        },
+        {
+          ...updateUserDto,
+        },
+        {
+          new: true,
+        },
+      );
+      
+    } catch (error) {
+      console.error(error)
+    } 
   }
 }
